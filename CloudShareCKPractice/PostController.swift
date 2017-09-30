80df8f00c2c86a1e3b015851e303b6562990c1bd
Sohailbasha <shlbsh94@gmail.com>
2017-09-29 20:44:12 -0400

    Created a Model Object Controller called "PostController"

---
+++ CloudShareCKPractice/PostController.swift
@@ -0,0 +1,16 @@
+//
+//  PostController.swift
+//  CloudShareCKPractice
+//
+//  Created by Ilias Basha on 9/29/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
+
+
+class PostController {
+    
+    
+    
+}
