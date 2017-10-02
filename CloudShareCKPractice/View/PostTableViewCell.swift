7e74c09710a8feb7c18540f2eb1a5377011e4421
Sohailbasha <shlbsh94@gmail.com>
2017-10-02 13:12:38 -0400

    added PostTableViewCell custom UITableViewCell class, grouped inside a View folder.

---
+++ CloudShareCKPractice/View/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  CloudShareCKPractice
+//
+//  Created by Ilias Basha on 10/2/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
