f2a424de9cd7293826a3d70e6c3a49c64caf0fb2
Sohailbasha <shlbsh94@gmail.com>
2017-10-02 13:35:02 -0400

    added post detail tableview controller + wired up the view's with their respective view controllers.

--- CloudShareCKPractice/View Controllers/PostViewController.swift
+++ CloudShareCKPractice/View Controllers/PostViewController.swift
@@ -25,15 +25,20 @@ class PostViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showPostDetail" {
+            if let destinationVC = segue.destination as? PostDetailTableViewController {
+                if let indexPath = tableView.indexPathForSelectedRow {
+                    let post = PostController.sharedController.posts[indexPath.row]
+                    destinationVC.post = post
+                }
+            }
+        }
     }
-    */
+
 
 }
 
