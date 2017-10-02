4bac58493ea2ba51ea0c3fbe8f069682baa230a5
Sohailbasha <shlbsh94@gmail.com>
2017-10-02 13:10:24 -0400

    added new group (Model Controller)

---
+++ CloudShareCKPractice/Model Controller/PostController.swift
@@ -0,0 +1,28 @@
+//
+//  PostController.swift
+//  CloudShareCKPractice
+//
+//  Created by Ilias Basha on 9/29/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class PostController {
+    
+    let sharedController = PostController()
+    let posts: [Post]? = []
+    
+    
+    func createPostWith(image: UIImage, with caption: String) {
+        let post = Post(photoData: UIImagePNGRepresentation(image))
+        addComment(caption, to: post)
+    }
+    
+    
+    private func addComment(_ text: String, to post: Post) {
+        let comment = Comment(text: text, post: post)
+        post.comments.append(comment)
+    }
+    
+}
