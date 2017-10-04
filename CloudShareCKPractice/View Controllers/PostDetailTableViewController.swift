04aab313ca2da214fc6745bc038179b3f82d3f8f
Sohailbasha <shlbsh94@gmail.com>
2017-10-03 20:51:22 -0400

    Wiring up PostDetailViewController and implementing delegate methods.

--- CloudShareCKPractice/View Controllers/PostDetailTableViewController.swift
+++ CloudShareCKPractice/View Controllers/PostDetailTableViewController.swift
@@ -11,70 +11,36 @@ import UIKit
 class PostDetailTableViewController: UITableViewController {
 
     var post: Post?
+    @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.rowHeight = 40
+        self.tableView.estimatedRowHeight = 800
     }
 
 
-
+    func updateViews() {
+        guard let post = self.post else { return }
+        imageView.image = post.photo
+        
+    }
+    
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return post?.comments.count ?? 0
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "comments", for: indexPath)
+        guard let timeStamp = post?.comments[indexPath.row].timeStamp, let commentText = post?.comments[indexPath.row].text else { return UITableViewCell() }
+        cell.textLabel?.text = "\(timeStamp)"
+        cell.detailTextLabel?.text = commentText
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -85,5 +51,34 @@ class PostDetailTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBAction func commentTapped(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Add Comment", message: "", preferredStyle: .alert)
+        var textFieldText: UITextField?
+        
+        alert.addTextField { (textField) in
+            textField.keyboardType = .default
+            textFieldText?.text = textField.text
+        }
+        
+        
+    }
+    
+    @IBAction func shareTapped(_ sender: Any) {
+    }
+    
+    @IBAction func followTapped(_ sender: Any) {
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
