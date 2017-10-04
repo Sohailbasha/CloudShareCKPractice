e7a48a192fc00d39c98641c6183db19e51e35469
Sohailbasha <shlbsh94@gmail.com>
2017-10-04 14:37:33 -0400

    Added functionality to cancel, add, and select photo buttons in Add Post Table View Controller.

--- CloudShareCKPractice/View Controllers/AddPostTableViewController.swift
+++ CloudShareCKPractice/View Controllers/AddPostTableViewController.swift
@@ -21,16 +21,31 @@ class AddPostTableViewController: UITableViewController {
     @IBOutlet var imageView: UIImageView!
     
     @IBAction func selectPhotoButtonTapped(_ sender: Any) {
-        
+        selectPhotoButton.setTitle("", for: .normal)
     }
     
     
     @IBAction func addPostButtonTapped(_ sender: Any) {
-        
+        if let image = imageView.image,
+            let text = textField.text, !text.isEmpty {
+            PostController.sharedController.createPostWith(image: image, with: text)
+        } else {
+            showAlert()
+        }
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
-        
+        self.dismiss(animated: true) {
+            self.imageView.image = nil
+            self.textField.text = ""
+            self.selectPhotoButton.setTitle("Select Photo", for: .normal)
+        }
     }
     
+    private func showAlert() {
+        let alert = UIAlertController(title: "Empty Values", message: "Fill in empty feilds to post.", preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        alert.addAction(dismiss)
+        present(alert, animated: true, completion: nil)
+    }
 }
